33f9aa2580a0bb88bd815375e5be7f3536e1d347
andreuccio88 <45309313+andreuccio88@users.noreply.github.com>
2022-12-19 20:32:28 +0100

    ok

--- 4_1_pfle_decomp.R
+++ 4_1_pfle_decomp.R
@@ -8,11 +8,33 @@ library('data.table')
 library(tidyverse)
 library(reshape2)
 
-load("Final.RData")
+load("Total.RData")
 
-unique(Final$CNT)
+head(Total)
+Total$Age<- 0
+Total$Age[Total$Ageclass==1] <- 5 
+Total$Age[Total$Ageclass==2] <- 10  
+Total$Age[Total$Ageclass==3] <- 15 
+Total$Age[Total$Ageclass==4] <- 20  
+Total$Age[Total$Ageclass==5] <- 25  
+Total$Age[Total$Ageclass==6] <- 30  
+Total$Age[Total$Ageclass==7] <- 35  
+Total$Age[Total$Ageclass==8] <- 40  
+Total$Age[Total$Ageclass==9] <- 45  
+Total$Age[Total$Ageclass==10] <- 50  
+Total$Age[Total$Ageclass==11] <- 55  
+Total$Age[Total$Ageclass==12] <- 60 
+Total$Age[Total$Ageclass==13] <- 65 
+Total$Age[Total$Ageclass==14] <- 70 
+Total$Age[Total$Ageclass==15] <- 75 
+Total$Age[Total$Ageclass==16] <- 80 
 
-Sullivan.fun = function (rates,age=AGE, sex=Gender) {
+
+
+
+
+
+Sullivan.fun = function (rates,age=AGE,sex="Male") {
   # 1) First, we split from our single vector 'rates' the set of age-specific death rates (mx) and age-specific prevalence of disability (wx)
   lengthvec <- length(rates)
   mx <- rates[1:(lengthvec / 2)]
@@ -64,8 +86,8 @@ Sullivan.fun = function (rates,age=AGE, sex=Gender) {
 
 
 #only if ages are not equal spaced
-start.age <- 60
-open.age <- 85
+start.age <- 0
+open.age <- 80
 
 library(tidyverse)
 library(dplyr)
@@ -74,23 +96,22 @@ library(dplyr)
 AGE=c(seq(start.age,open.age,5))
 
 
-year1 = 2005; year2 = 2016;G="Female";country="ITA"
+year1 = 2007; year2 = 2019;country="IT"
 
-str(Final)
-DEC_RES=function(start.age, open.age, Gender,
+str(Total)
+DEC_RES=function(start.age, open.age, 
                  year1, year2, country){
   
-  filter(Final,Year==year1 & Ageclass >=start.age & Gender == G & CNT == country)
-  Final$Gender <- as.factor(Final$Gender)
-  Final$Gender
+  filter(Total,Year==year1 & Age >=start.age & CNT == country)
+
   # 5) Getting the female death rates for ages >=65 in 1970 and 1990
-  mx1 <- filter(Final,Year==year1 & Ageclass >=start.age & Gender == G & CNT == country)$mx
-  mx2 <- filter(Final,Year==year2 & Ageclass >=start.age & Gender == G & CNT == country)$mx
+  mx1 <- filter(Total,Year==year1 & Age >=start.age &  CNT == country)$mx
+  mx2 <- filter(Total,Year==year2 & Age >=start.age &  CNT == country)$mx
   # 6) We considered 85+ as the open age interval to match the disability data
   
   
-  wx1 <- filter(Final,Year==year1 & Ageclass >=start.age & Gender == G & CNT == country)$Prevalence
-  wx2 <- filter(Final,Year==year2 & Ageclass >=start.age & Gender == G & CNT == country)$Prevalence
+  wx1 <- filter(Total,Year==year1 & Age >=start.age &  CNT == country)$HCR
+  wx2 <- filter(Total,Year==year2 & Age >=start.age &  CNT == country)$HCR
   # 6) We considered 85+ as the open age interval to match the disability data
   
   
@@ -104,13 +125,12 @@ DEC_RES=function(start.age, open.age, Gender,
     func=Sullivan.fun,
     pars1 = mxwx1,
     pars2 = mxwx2,
-    sex=Gender,  
     N=100)
   
   
   HE_cont <- matrix(HE_Decomp_Cont,nrow=(length(HE_Decomp_Cont)/2),ncol=2,
                     byrow=F)
-  colnames(HE_cont) <- c("Mortality","Morbidity")
+  colnames(HE_cont) <- c("Mortality","Poverty")
   # 2) Creating a data frame with the matrices and adding
   # a column with the beginning of the age interval
   
@@ -126,24 +146,26 @@ DEC_RES=function(start.age, open.age, Gender,
 
 AGE=c(seq(start.age,open.age,5))
 
-DEC_RES(start.age =60,
-        open.age = 85,year1 = year1,
-        year2 = year2, Gender=G,country="ITA")
+DEC_RES(start.age =0,
+        open.age = 80,year1 = year1,
+        year2 = year2, country="DE")
 
+
+Total$CNT
 # 
-country <- c("AUT","BEL","DNK","EST","FIN","FRACNP","GRC",
-             "IRL","ITA","LUX","PRT","ESP","SWE","CZE",
-             "DEUTNP","HUN","LVA","LTU", "NLD","POL","SVK",
-             "SVN","GBR_NP","BGR")
 
+country <- unique(Total$CNT)
 
+country <-country[c(1,3,4,5)]
+#country <- c("IT","FR")
 #  
 #  country <- c("AUT","BEL","DNK","FIN","FRACNP",
 #               "DEUTNP","GRC","IRL","ITA","LUX",
 #               "NLD", "PRT","ESP", "SWE", "GBR_NP")
 # # 
 
-
+year1 =2007
+year2 = 2019
 Res <- list()
 for (i in 1:length(country)) {
   cnt <- country[i]
@@ -151,7 +173,7 @@ for (i in 1:length(country)) {
   
   Res[[cnt]] <-  DEC_RES(start.age =start.age,
                          open.age = open.age,year1 =year1 ,
-                         year2 = year2, Gender=G,country=cnt)
+                         year2 = year2,country=cnt)
   
 }
 
@@ -165,8 +187,10 @@ TOT <- ggplot(data=HE_cont_res, aes(x=as.factor(Age),
                                     y=Contribution, fill =type))+
   geom_bar(stat = "identity", position = "stack")+coord_flip()+facet_wrap(~CNT)+
   #scale_fill_manual(values=c("grey30", "grey60"))+
-  scale_x_discrete(labels=c("60-64","65-69","70-74","75-79","80-84","85+"))+
-  geom_hline(yintercept=0, linetype="dashed", color = "gray50", size=0.5)+ggtitle("Female population") + xlab("Age")+
+  scale_x_discrete(labels=c("0-4","5-9","10-14","15-19","20-24","25-29",
+                            "30-34","35-39","40-44","45-49","50-54","55-59",
+                            "60-64","65-69","70-74","75-79","80-84","85+"))+
+  geom_hline(yintercept=0, linetype="dashed", color = "gray50", size=0.5)+ggtitle("Total population") + xlab("Age")+
   #scale_y_continuous(breaks=seq(-0.1,0.3,by=0.05))+
   # xlab("Age-group")+annotate("text", x=3, y=0.2, 
   #                            label=paste("HLE", year1,"=", round(Sullivan.fun(rates=mxwx1,sex = Gender),2)),fontface =1)+
@@ -182,30 +206,30 @@ TOT <- ggplot(data=HE_cont_res, aes(x=as.factor(Age),
 
 
 TOT
-ggsave("hle65_female.pdf",TOT, width = 15, height = 10)
+ggsave("PF_LE.pdf",TOT, width = 15, height = 10)
 
 hle_F <- HE_cont_res
 
-save(hle_F, file = "hle_F.RData")
+save(hle_F, file = "PFLE.RData")
 
-###############
-# aggr ages
-###############
-
-library(tidyverse)
-
-load("hle_F.RData")
-
-head(hle_F)
-hle_F$Ageclass2 <- NA
-hle_F$Ageclass2[hle_F$Age%in%c(60,65)] <- "60-69"
-hle_F$Ageclass2[hle_F$Age%in%c(70,75)] <- "70-79"
-hle_F$Ageclass2[hle_F$Age>=80] <- "80+"
-
-
-hle_F_Age2 <- aggregate(hle_F$Contribution,by = list(hle_F$Ageclass2,hle_F$CNT, hle_F$type),FUN=sum) %>% 
-  rename(Ageclass=Group.1,CNT=Group.2, Type=Group.3, Contribution=x) 
-
-head(hle_F_Age2)
-save(hle_F_Age2,file = "hle_F_Age2.RData")
-
+# ###############
+# # aggr ages
+# ###############
+# 
+# library(tidyverse)
+# 
+# load("hle_F.RData")
+# 
+# head(hle_F)
+# hle_F$Ageclass2 <- NA
+# hle_F$Ageclass2[hle_F$Age%in%c(60,65)] <- "60-69"
+# hle_F$Ageclass2[hle_F$Age%in%c(70,75)] <- "70-79"
+# hle_F$Ageclass2[hle_F$Age>=80] <- "80+"
+# 
+# 
+# hle_F_Age2 <- aggregate(hle_F$Contribution,by = list(hle_F$Ageclass2,hle_F$CNT, hle_F$type),FUN=sum) %>% 
+#   rename(Ageclass=Group.1,CNT=Group.2, Type=Group.3, Contribution=x) 
+# 
+# head(hle_F_Age2)
+# save(hle_F_Age2,file = "hle_F_Age2.RData")
+# 
